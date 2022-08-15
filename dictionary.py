310812df90f4aaf406ecc5d1fa73102ab34da4a4
Jay Prakash Sharma <jayprakashsharma225@gmail.com>
2022-08-15 23:47:50 +0530

    Create dictionary.py
    Added the dictionary.

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+mydict = {
+     "uno" : "one",
+     "dos" : "two",
+     "tres" : "three",
+     "cuatro" : "four",
+     "cinco" : "five",
+     "seis" : "seven",
+     "siete" : "eight",
+      "ocho" : "nine",
+      "diez" : "ten"
+}
